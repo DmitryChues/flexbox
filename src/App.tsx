4ca51d84f7461127f4080f608a6b08fe27ffea34
Dmitry <dmitrychues2312@gmail.com>
2023-12-10 23:55:31 +0300

    flex

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="flex-container">
+				<div className="myDiv">I'm div</div>
+				<img className="myImg" src="https://i.pinimg.com/236x/0e/bd/26/0ebd262c4b7f69f7ec915dbd8509328f.jpg" alt="image" />
+				<input className='myInput' type="text" placeholder="I'm input" />
+				<p className="myText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus ipsum aspernatur odio, fuga tenetur minus dolore? Dolor, dicta nulla? Quos vero non illum qui adipisci aperiam culpa corrupti esse numquam! Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+				<span className="mySpan">I'm span</span>
+			</div>
+		</div>
+	);
 }
 
 export default App;
